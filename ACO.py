589c81a32b0b9e79f312588fd41530eae3891dda
Nishant Sinha <nishant15066@iiitd.ac.in>
2018-11-09 07:45:59 +0530

    Compiled the ACO Observations and report file.

--- ACO.py
+++ ACO.py
@@ -15,19 +15,16 @@ def create_graph(matrix, n):
     G = nx.Graph()
     for i in range(n):
         for j in range(n):
+            if i == j:
+                continue
             G.add_edge(i, j)
             G[i][j]['weight'] = matrix[i][j]
     nx.draw(G, with_labels=True)
-    plt.show()
-
-
-n = 5
-matrix = create_distance_matrix(n)
-create_graph(matrix, n)
-
+    # plt.savefig('visualize'+str(n)+'.png')
+    plt.clf()
 
 class AntColony:
-    def __init__(self, distance, n = 5, l = 10, alpha = 1, beta = 3, rho = 0.8, Q = 1, iterations = 100):
+    def __init__(self, distance, n=4, l=10, alpha=1, beta=3, rho=0.8, Q=1, iterations=100):
         self.tau = [[0 for i in range(n)] for j in range(n)]
         self.cities = n
         self.ants = l
@@ -47,29 +44,35 @@ class AntColony:
         for i in range(self.cities):
             for j in range(self.cities):
                 if i != j:
-                    visibility[i][j] = 1/self.distance[i][j]
+                    visibility[i][j] = 1 / self.distance[i][j]
         return visibility
 
-    def ant(self, ant_id):
-        prev_city = np.random.randint(0, high = self.cities)
+    def ant(self, ant_id, iter):
+        prev_city = np.random.randint(0, high=self.cities)
+        next_city = np.random.randint(0, high=self.cities)
         distance = 0
         self.visited[ant_id].append(prev_city)
-        for i in range(self.cities):
-            next_city = self.move(prev_city, ant_id)
+        for i in range(self.cities-1):
+            if iter == 0:
+                while next_city == prev_city:
+                    next_city = np.random.randint(0, high=self.cities)
+            else:
+                next_city = self.move(prev_city, ant_id)
             distance += self.distance[prev_city][next_city]
             self.path[ant_id].append((prev_city, next_city))
             self.visited[ant_id].append(next_city)
+            prev_city = next_city
         self.tour_length[ant_id] = distance
 
     def move(self, prev_city, ant_id):
         probability = [0 for i in range(self.cities)]
         for j in range(self.cities):
             if j not in self.visited[ant_id]:
-                probability[j] = math.power(self.tau[ant_id][prev_city][j], self.alpha) * math.power(
-                    self.visibility[ant_id][prev_city][j], self.beta)
+                probability[j] = math.pow(self.tau[prev_city][j], self.alpha) * math.pow(
+                    self.visibility[prev_city][j], self.beta)
             else:
                 probability[j] = 0
-        next_city = np.random.choice(a = [i for i in range(self.cities)], p = probability)
+        next_city = np.random.choice(a=[i for i in range(self.cities)], p=np.divide(probability, sum(probability)))
         return next_city
 
     def update_tau(self):
@@ -79,19 +82,39 @@ class AntColony:
                     continue
                 delta = 0
                 for ant_id in range(self.ants):
-                    if (i,j) in self.path[ant_id] or (j,i) in self.path[ant_id]:
-                        delta += self.Q/self.tour_length[ant_id]
-                self.tau[i][j] = self.rho*self.tau[i][j] + delta
+                    if (i, j) in self.path[ant_id] or (j, i) in self.path[ant_id]:
+                        delta += self.Q / self.tour_length[ant_id]
+                self.tau[i][j] = self.rho * self.tau[i][j] + delta
 
     def reset(self):
-        return None
+        self.tour_length = [0 for i in range(self.ants)]
+        self.path = [[] for i in range(self.ants)]
+        self.visited = [[] for i in range(self.ants)]
 
     def solve(self):
+        temp = []
         for i in range(self.iterations):
             self.reset()
             for ant_id in range(self.ants):
-                self.ant(ant_id)
+                self.ant(ant_id, i)
             self.update_tau()
-            print(self.tour_length)
-        i = np.argmin(self.tour_length)
-        print(self.path[i])
+            print(sum(self.tour_length) / len(self.tour_length))
+            temp.append(sum(self.tour_length) / len(self.tour_length))
+        print("Printing path")
+        for i in self.path:
+            print(i)
+        self.plot(temp)
+
+    def plot(self, a):
+        plt.plot(a)
+        plt.show()
+
+# for i in range(3, 10):
+#     matrix = create_distance_matrix(i)
+#     create_graph(matrix, i)
+i = 5
+n = 5
+matrix = create_distance_matrix(i)
+create_graph(matrix, i)
+a = AntColony(matrix)
+a.solve()
